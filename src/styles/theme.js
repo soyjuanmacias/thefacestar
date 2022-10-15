08d86eba6a98367238a76f46fc895db2bfd9a324
Juan <info@juanmacias.es>
2022-10-15 04:51:41 +0200

    FIX: style theme font size

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,11 +2,12 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
-  fonts: {
-    logo: `'copperplate', sans-serif`,
-    // heading: `'Heading Font Name', sans-serif`,
-    // body: `'Body Font Name', sans-serif`,
-  },
+  Navbar: {
+    fonts: {
+      heading: `'copperplate', sans-serif`,
+      // body: `'Body Font Name', sans-serif`,
+    },
+  }
 })
 
 export default theme
